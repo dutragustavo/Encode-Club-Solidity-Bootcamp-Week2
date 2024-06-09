f3cb568bb457e867474f86025ed0b91a80ca4985
Gustavo Dutra <gobannondutra@gmail.com>
2024-06-09 20:04:03 -0300

    fix: hardhat config missing dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const providerApiKey = process.env.ALCHEMY_API_KEY || "";
 const deployerPrivateKey = process.env.PRIVATE_KEY || "";
 
